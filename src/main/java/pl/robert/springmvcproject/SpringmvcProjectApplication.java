3397713eac214f78dd57c45a7132c7ffd90a1c3a
RobertK <robert.krzywina@gmail.com>
2018-10-31 16:06:59 +0100

    fixed style.css and added some look

--- src/main/java/pl/robert/springmvcproject/SpringmvcProjectApplication.java
+++ src/main/java/pl/robert/springmvcproject/SpringmvcProjectApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"controller", "repository"})
 @EntityScan(basePackages = "model")
 @EnableJpaRepositories(basePackages = "repository")
-public class SpringmvcProjectApplication {
+public class SpringmvcProjectApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcProjectApplication.class, args);
     }
